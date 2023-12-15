7c51e30d6563d9ca877642f558c529517d923ed9
hwn0809 <hwn0809@gmail.com>
2023-12-15 15:34:59 +0900

    add main

--- ecs_fargate/alb.tf
+++ ecs_fargate/alb.tf
@@ -14,9 +14,7 @@ resource "aws_alb_target_group" "main" {
   name  = "${var.project_name}-${var.environment}-tg-${element(local.target_groups, count.index)}"
   tags  = merge(local.tags, { Name = "${title(var.project_name)} Target Group" })
 
-  # port        = 443
-  # protocol    = "HTTP"
-  port        = 80
+  port        = 443
   protocol    = "HTTP"
   vpc_id      = aws_vpc.main.id
   target_type = "ip"
@@ -34,46 +32,46 @@ resource "aws_alb_listener" "main" {
   protocol          = "HTTP"
   tags              = merge(local.tags, { Name = "${title(var.project_name)} Listener" })
 
-  # default_action {
-  #   type = "redirect"
+  default_action {
+    type = "redirect"
 
-  #   redirect {
-  #     port        = "443"
-  #     protocol    = "HTTPS"
-  #     status_code = "HTTP_301"
-  #   }
-  # }
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
   default_action {
     type = "forward"
     target_group_arn = element(aws_alb_target_group.main.*.arn, 0)
   }
 }
 
-# resource "aws_alb_listener" "prod" {
-#   load_balancer_arn = aws_alb.main.arn
-#   port              = 443
-#   protocol          = "HTTPS"
-#   ssl_policy        = var.ssl_policy
-#   certificate_arn   = var.certificate_arn
-#   tags              = merge(local.tags, { Name = "${title(var.project_name)} Listener" })
+resource "aws_alb_listener" "prod" {
+  load_balancer_arn = aws_alb.main.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = var.ssl_policy
+  certificate_arn   = var.certificate_arn
+  tags              = merge(local.tags, { Name = "${title(var.project_name)} Listener" })
 
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_alb_target_group.main.1.arn
-#   }
-# }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_alb_target_group.main.1.arn
+  }
+}
 
 
-# resource "aws_alb_listener" "test" {
-#   load_balancer_arn = aws_alb.main.arn
-#   port              = 8443
-#   protocol          = "HTTPS"
-#   ssl_policy        = var.ssl_policy
-#   certificate_arn   = var.certificate_arn
-#   tags              = merge(local.tags, { Name = "${title(var.project_name)} Listener" })
+resource "aws_alb_listener" "test" {
+  load_balancer_arn = aws_alb.main.arn
+  port              = 8443
+  protocol          = "HTTPS"
+  ssl_policy        = var.ssl_policy
+  certificate_arn   = var.certificate_arn
+  tags              = merge(local.tags, { Name = "${title(var.project_name)} Listener" })
 
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_alb_target_group.main.1.arn
-#   }
-# }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_alb_target_group.main.1.arn
+  }
+}
